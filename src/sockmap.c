d36b04bd1b5ea6c1f2c8944d31ae56919c7a8f0c
Konrad Sztyber <konrad.sztyber@intel.com>
2021-03-23 13:35:20 +0100

    sockmap: minimal example of the sockmap BPF interface
    The program accepts 3 connections and then splits the data from the
    first one to the other two.  The data is split on a packet boundary and
    the packets are alternated betwen the connections.  So for multiple
    packets it looks like this:
     - packet 0 -> connection 1,
     - packet 1 -> connection 2,
     - packet 2 -> connection 1,
     - packet 3 -> connection 2, etc.
    
    The easiest way to test it is using netcat:
    
    [shell 0] $ src/sockmap 127.0.0.1 1024
    [shell 1] $ nc 127.0.0.1 1024
    [shell 2] $ nc 127.0.0.1 1024
    [shell 3] $ nc 127.0.0.1 1024
    
    Writing something on shell #1 will cause it to be redirected and output
    to shell #2.  A second write will be redirectd it to shell #3, another
    one to shell #2, then #3, etc.
    
    Signed-off-by: Konrad Sztyber <konrad.sztyber@intel.com>

---
+++ src/sockmap.c
@@ -0,0 +1,196 @@
+#define _GNU_SOURCE
+#include <bpf/bpf.h>
+#include <bpf/libbpf.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <poll.h>
+#include <signal.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/un.h>
+#include <unistd.h>
+#include "sockmap.skel.h"
+
+#define ARRAY_SIZE(arr) ((sizeof(arr) / sizeof((arr)[0])))
+
+volatile bool running = true;
+
+static int
+libbpf_print_fn(enum libbpf_print_level level, const char *format, va_list args)
+{
+	return vfprintf(stderr, format, args);
+}
+
+static int
+init_socket(int family, struct sockaddr *addr, size_t addrlen)
+{
+	int fd, en = 1;
+
+	fd = socket(family, SOCK_STREAM, 0);
+	if (fd < 0) {
+		fprintf(stderr, "socket() failed\n");
+		goto error;
+	}
+
+	setsockopt(fd, SOL_SOCKET, SO_REUSEADDR, &en, sizeof(en));
+	if (bind(fd, addr, addrlen) != 0) {
+		fprintf(stderr, "bind() failed\n");
+		goto error;
+	}
+
+	if (listen(fd, 1024) < 0) {
+		fprintf(stderr, "listen() failed\n");
+		goto error;
+	}
+
+	return fd;
+error:
+	close(fd);
+	return -1;
+}
+
+static void
+handle_connection(struct sockmap_bpf *skel, int fd, int *split_fd)
+{
+	struct pollfd poll_fd;
+	int rc, i, errsv, map_off = 0;
+
+	/* Update the element with the input fd to attach the parse/verdict progs to that fd */
+	rc = bpf_map_update_elem(bpf_map__fd(skel->maps.sock_map), &map_off, &fd, BPF_ANY);
+	if (rc != 0) {
+		errsv = errno;
+		fprintf(stderr, "bpf_map_update_elem() failed: %s\n",
+			strerror(errsv));
+		return;
+	}
+	map_off++;
+	for (i = 0; i < 2; ++i) {
+		rc = bpf_map_update_elem(bpf_map__fd(skel->maps.sock_map), &map_off, &split_fd[i],
+					 BPF_ANY);
+		if (rc != 0) {
+			errsv = errno;
+			fprintf(stderr, "bpf_map_update_elem() failed (%d): %s\n",
+				i, strerror(errsv));
+			return;
+		}
+
+		map_off++;
+	}
+
+	/* Wait until the connection is closed */
+	poll_fd.fd = fd;
+	poll_fd.events = POLLRDHUP;
+	poll(&poll_fd, 1, -1);
+}
+
+static void
+signal_handler(int sig)
+{
+	running = false;
+}
+
+int
+main(int argc, const char **argv)
+{
+	struct sockmap_bpf *skel;
+	struct pollfd poll_fd;
+	struct addrinfo hints = { 0 }, *ai;
+	int i, client_fd[3], num_clients = 0;
+	int rc = 0, fd = -1;
+
+	if (argc != 2) {
+		fprintf(stderr, "Usage: %s PORT\n", argv[0]);
+		return 1;
+	}
+
+	hints.ai_family = AF_INET;
+	hints.ai_socktype = SOCK_STREAM;
+	rc = getaddrinfo("127.0.0.1", argv[1], &hints, &ai);
+	if (rc != 0) {
+		fprintf(stderr, "Failed to parse the port: %s\n", argv[1]);
+		return 1;
+	}
+
+	signal(SIGINT, signal_handler);
+	libbpf_set_print(libbpf_print_fn);
+
+	skel = sockmap_bpf__open();
+	if (!skel) {
+		fprintf(stderr, "Failed to open BPF skeleton\n");
+		return 1;
+	}
+
+	rc = sockmap_bpf__load(skel);
+	if (rc != 0) {
+		fprintf(stderr, "Failed to load and verify BPF skeleton\n");
+		goto cleanup;
+	}
+
+	rc = sockmap_bpf__attach(skel);
+	if (rc != 0) {
+		fprintf(stderr, "Failed to attach BPF skeleton\n");
+		goto cleanup;
+	}
+
+	rc = bpf_prog_attach(bpf_program__fd(skel->progs.sk_parser),
+			     bpf_map__fd(skel->maps.sock_map),
+			     BPF_SK_SKB_STREAM_PARSER, 0);
+	if (rc != 0) {
+		fprintf(stderr, "bpf_prog_attach() failed: %s\n", strerror(errno));
+		goto cleanup;
+	}
+
+	rc = bpf_prog_attach(bpf_program__fd(skel->progs.sk_verdict),
+			     bpf_map__fd(skel->maps.sock_map),
+			     BPF_SK_SKB_STREAM_VERDICT, 0);
+	if (rc != 0) {
+		fprintf(stderr, "bpf_prog_attach() failed: %s\n", strerror(errno));
+		goto cleanup;
+	}
+
+	fd = init_socket(AF_INET, ai->ai_addr, ai->ai_addrlen);
+	if (fd < 0) {
+		goto cleanup;
+	}
+
+	poll_fd.fd = fd;
+	poll_fd.events = POLLIN;
+
+	while (running) {
+		rc = poll(&poll_fd, 1, 1000);
+		if (rc < 0) {
+			if (errno != EINTR) {
+				fprintf(stderr, "poll() failed\n");
+			}
+
+			break;
+		} else if (rc == 0) {
+			continue;
+		}
+
+		rc = accept(fd, NULL, NULL);
+		if (rc < 0) {
+			fprintf(stderr, "accept() failed\n");
+			goto cleanup;
+		}
+
+		client_fd[num_clients++] = rc;
+		if (num_clients == ARRAY_SIZE(client_fd)) {
+			handle_connection(skel, client_fd[0], &client_fd[1]);
+			for (i = 0; i < ARRAY_SIZE(client_fd); ++i) {
+				close(client_fd[i]);
+			}
+
+		}
+	}
+cleanup:
+	if (fd >= 0) {
+		close(fd);
+	}
+	sockmap_bpf__destroy(skel);
+	return -rc;
+}
