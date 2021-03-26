3357b5283ec7a2639c1b16f64805b5d3f6dfe804
Konrad Sztyber <konrad.sztyber@intel.com>
2021-03-26 07:30:38 +0100

    nvmf: initial definitions for NVMeoF/TCP parser
    For now, this is just a copy of the sockmap example, but it'll be
    updated to parse and split the incoming NVMeoF/TCP stream putting the
    control data in one socket and the payload in the other.
    
    Signed-off-by: Konrad Sztyber <konrad.sztyber@intel.com>

---
+++ src/nvmf.bpf.c
@@ -0,0 +1,32 @@
+#include "vmlinux.h"
+#include <bpf/bpf_helpers.h>
+
+char LICENSE[] SEC("license") = "Dual BSD/GPL";
+
+struct {
+	__uint(type, BPF_MAP_TYPE_SOCKMAP);
+#define MAX_ENTRIES 3
+	__uint(max_entries, MAX_ENTRIES);
+	__type(key, int);
+	__type(value, int);
+} sock_map SEC(".maps");
+
+SEC("sk_skb/stream_parser")
+int sk_parser(struct __sk_buff *skb)
+{
+	bpf_printk("parser: %d (%p)", skb->len, skb);
+	return skb->len;
+}
+
+uint32_t current;
+
+SEC("sk_skb/stream_verdict")
+int sk_verdict(struct __sk_buff *skb)
+{
+	uint32_t idx = current + 1;
+
+	current = idx % (MAX_ENTRIES - 1);
+
+	bpf_printk("verdict: %u, %d (%p)", idx, skb->len, skb);
+	return bpf_sk_redirect_map(skb, &sock_map, idx, 0);
+}
